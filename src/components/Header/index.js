d72c5e1d0cf4e60a72a97fd93d8d2e1374e4595c
kelbri10 <kelbri10@gmail.com>
2020-11-02 20:12:00 -0500

    added header, imported searchbar component to appear in header

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import SearchBar from '../SearchBar';
+
+
+export default function Header() { 
+    return ( 
+        <div className='header'>
+            <h1>myUserDirect</h1>
+            <SearchBar />
+        </div>
+    )
+}
