9ff7d7158371f7b5f7e2bb893bde331fc31b3c23
kelbri10 <kelbri10@gmail.com>
2020-11-02 20:46:22 -0500

    added table imported employeebody and employeeInfo to employeetable

---
+++ src/components/EmployeeTable/index.js
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react'; 
+import EmployeeInfoContext from 'src/components/EmployeeInfo'; 
+import EmployeeBody from '../EmployeeBody';
+
+export default function EmployeeTable() { 
+    const context = useContext(EmployeeInfoContext); 
+
+    return ( 
+        <div className='datatable mt-5'>
+            <table
+                id='table'
+                className='table table-striped table-hover table-condensed'
+            >
+                <thead>
+                    <tr>
+                        {context.employeeState.headings.map(({ name, width}) => {
+                            return (
+                                <th
+                                    className='col'
+                                    key={name}
+                                    style={{ width }}
+                                    onClick={() => {
+                                        context.handleSort(name.toLowerCase()); 
+                                    }}>
+                                        {name}
+                                        <span className='pointer'></span>
+                                </th>
+                            )
+                        })}
+                    </tr>
+                </thead>
+
+                <EmployeeBody />
+            </table>
+        </div>
+    );
+}
