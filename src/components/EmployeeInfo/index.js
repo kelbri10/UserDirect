2864c5611a47ae64934e48f1efe949ea43605bd4
kelbri10 <kelbri10@gmail.com>
2020-11-02 21:40:55 -0500

    updated readMe, debugged axios call in Employee Info

--- src/components/EmployeeInfo/index.js
+++ src/components/EmployeeInfo/index.js
@@ -83,7 +83,7 @@ export default function EmployeeInfo() {
                 filteredUsers: results.data.results
             }); 
         }); 
-    }); 
+    }, []); 
 
     return (
         <EmployeeInfoContext.Provider 
