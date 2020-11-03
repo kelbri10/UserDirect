8783e6dffabc75566d1d9ca62058ef6112227cfd
kelbri10 <kelbri10@gmail.com>
2020-11-02 20:00:29 -0500

    imported EmployeeInfoContext to SearchBar, added form component

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react'; 
+import EmployeeInfoContext from 'src/utils/EmployeeContextInfo';
+
+export default function SearchBar() { 
+    const context = useContext(EmployeeInfoContext); 
+
+    return (
+        <div className='searchBar'>
+            <form className='form-inline'>
+
+            <input 
+                placeholder='enter name'
+                type='text'
+                className='form-control'
+                aria-label='search'
+                id='name'
+                onChange={name=> context.handleSearchChange(name)}
+            />
+
+            <button
+                className='btn btn-outline-secondary'
+                type='submit'>
+                    Search
+            </button>
+
+            </form>
+        </div>
+    );
+}
